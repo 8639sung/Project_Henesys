f021e9ef8bee8516004d57ac4ae226525d37678c
Hong-Bin Yoon <hongbinyoon@gmail.com>
2021-05-08 17:33:50 -0700

    extendeding User model to HenesysUser

--- Henesys/accounts/models.py
+++ Henesys/accounts/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+# Henesys User Model Implementation
+# OneToOneField contains a reference to the User model in our HenesysUser model.
+
+class HenesysUser(models.Model):
+    # Associate HenesysUser model with User model
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE);
+    fk_name = "user"
+
+    # additional fields (see https://docs.djangoproject.com/en/3.2/ref/models/fields/)
+    stars = models.PositiveBigIntegerField(default=0)
+    mana  = models.PositiveBigIntegerField(default=0)
+
