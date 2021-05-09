31d4836412d216fa310950099a77f0a1a0e691f2
ZZIN <8639sung@gmail.com>
2021-05-09 02:02:31 +0000

    loginpage

--- Henesys/accounts/models.py
+++ Henesys/accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=40000)
+    password = models.EmailField(max_length=40000)
+    password1 = models.CharField(max_length=40000)
+    password2 = models.CharField(max_length=40000)
